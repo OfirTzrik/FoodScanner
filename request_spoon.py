1f6949ec79707dff897845b87fe9a2f01f916d3d
OfirTzrik <ofirc36@gmail.com>
2025-08-19 13:42:08 +0300

    Fix access to recipes as json

--- request_spoon.py
+++ request_spoon.py
@@ -6,12 +6,16 @@ def requestRecipeSteps(ingredients: list, number_of_recipes: int):
     Return the information and steps of some n recipes
     Accepts the list of ingredients the user has and a number of recipes to retrieve
     Returned as a zip with each row being <information about the recipe, the recipe's steps>
+
+    zip_object = requestRecipeSteps([list of ingredients as strings], how many recipes to get)
+    for info, steps in zip_object:
+        ...
     """
     # Request the recipes based on the ingredients passed
     ingredients_str = ",+".join(ingredients)
     request_parameters = {"ingredients": ingredients_str, "number": number_of_recipes,"apiKey": st.secrets["API_KEY"]}
     try:
-        recipes = requests.get("https://api.spoonacular.com/recipes/findByIngredients", params=request_parameters)
+        recipes = requests.get("https://api.spoonacular.com/recipes/findByIngredients", params=request_parameters).json()
     # This happens when the daily points limit has been reached (too many requests from spoonacular)
     except Exception as e:
         return None

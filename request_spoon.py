8e420ad9f327e99ef7525ec31db5e289230f1d59
OfirTzrik <ofirc36@gmail.com>
2025-08-25 14:07:36 +0300

    Add function for recipe by nutrients
    Added the function requestRecipeByNutrients.
    Needs to be tested when the API usage resets.

--- request_spoon.py
+++ request_spoon.py
@@ -32,3 +32,45 @@ def requestRecipeSteps(ingredients: list, number_of_recipes: int):
         recipes_steps.append(steps)
 
     return zip(recipes, recipes_steps)
+
+def requestRecipeByNutrients(min_carbs: int = 0, max_carbs: int = 500,
+                             min_protein: int = 0, max_protein: int = 100,
+                             min_calories: int = 0, max_calories: int = 5000,
+                             min_fat: int = 0, max_fat: int = 150,
+                             number_of_recipes: int = 2):
+    """
+    Return the information and steps of some n recipes
+    Accepts nutrient information from the user and a number of recipes to retrieve
+    Returned as a zip with each row being <information about the recipe, the recipe's steps>
+
+    zip_object = requestRecipeSteps(..., ..., ..., how many recipes to get)
+    for info, steps in zip_object:
+        ...
+    """
+    request_parameters = {
+        "minCarbs": min_carbs, "maxCarbs": max_carbs,
+        "minProtein": min_protein, "maxProtein": max_protein,
+        "minCalories": min_calories, "maxCalories": max_calories,
+        "minFat": min_fat, "maxFat": max_fat,
+        "number": number_of_recipes,
+        "apiKey": st.secrets["API_KEY"]
+    }
+    # Request the recipes based on the nutrients passed
+    try:
+        recipes = requests.get("https://api.spoonacular.com/recipes/findByNutrients", params=request_parameters).json()
+    # This happens when the daily points limit has been reached (too many requests from spoonacular)
+    except Exception as e:
+        return None
+    
+    # Request the steps for each of the recipes received
+    recipes_steps = []
+    for recipe in recipes:
+        url = "https://api.spoonacular.com/recipes/" + str(recipe["id"]) + "/analyzedInstructions"
+        try:
+            steps = requests.get(url, params=request_parameters).json()
+        # This happens when the daily points limit has been reached (too many requests from spoonacular)
+        except Exception as e:
+            return None
+        recipes_steps.append(steps)
+
+    return zip(recipes, recipes_steps)

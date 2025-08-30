31fb82095e5d728b6551d66ad252b7535fd26c0e
OfirTzrik <ofirc36@gmail.com>
2025-08-30 17:24:16 +0300

    Add commenting where needed

--- homepage.py
+++ homepage.py
@@ -5,16 +5,18 @@ import request_spoon as rs
 st.header("Food Scanner", divider=True)
 
 search_options = st.selectbox("Choose search mode:", ["by ingredients", "by nutrients"])
-# The option to search food by the ingredients
 
 def showByIngredients():
+    """
+    Search for recipes based on the ingredients entered by the user.
+    Returns 2 recipes (Spoonacular's API (free tier) is limited to 50 points per day).
+    """
     if "items" not in st.session_state:
-        st.session_state.items = []
+        st.session_state.items = [] # Holds the list of ingredients entered
 
-    # input fields
+    # Enter ingredients and add to list
     ingredient_name = st.text_input("Enter item name:")
     ingredient_amount = st.number_input("Enter the amount of the ingredient:",min_value=1, step=1, value=1)
-    # a button the add the ingredient
     if st.button("Add to list"):
         if ingredient_name.strip():
             found = False
@@ -24,6 +26,7 @@ def showByIngredients():
                     found = True
                     break
             
+            # Ingredient was not found
             if not found:
                 st.session_state["items"].append({
                     "name": ingredient_name.strip().capitalize(),
@@ -32,8 +35,7 @@ def showByIngredients():
             
             st.success(f"Added: {ingredient_name}")
 
-    # options to increase or decrease or delete the amount of each ingredient
-
+    # Increase / decrease amount of each ingredient
     if st.session_state["items"]:
         st.subheader("List of ingredients:")
         for item in st.session_state["items"]:
@@ -49,10 +51,11 @@ def showByIngredients():
                 else:
                     st.warning(f"{item['name']} can’t be less than 1")
 
+            # Remove the ingredient from the list
             if delete.button("🗑️ Delete", key=f"delete_{item['name']}"):
                 st.session_state["items"].remove(item)
                 st.success(f"Deleted: {item['name']}")
-                st.rerun()  # refresh UI after deletion
+                st.rerun() # Refresh UI after deletion
 
     # Request recipes as long as at least one ingredient was entered
     if st.button("Get recipe(s)") and len(st.session_state["items"]) > 1:
@@ -66,6 +69,7 @@ def showByIngredients():
             info_col.subheader(info.get("title", "No title"))
             info_col.image(info.get("image", ""), use_container_width=True)
             info_col.markdown("📝 Missing ingredients")
+            # List the missing ingredients
             if "missedIngredients" in info:
                 for ing in info["missedIngredients"]:
                     col1, col2 = st.columns([1,4])
@@ -82,6 +86,10 @@ def showByIngredients():
                 steps_col.write("No instructions available.")
 
 def showByNutrients():
+    """
+    Search for recipes based on the nutrients values entered by the user.
+    returns 2 recipes (Spoonacular's API (free tier) is limited to 50 points per day).
+    """
     st.subheader("Set your nutrients values to filter:")
 
     # DELETE AFTER API USAGE RESETS AND THE REPLACEMENT CODE IS CHECKED
@@ -103,12 +111,12 @@ def showByNutrients():
     min_carbs, max_carbs = st.slider("Min to max carbs", 0, 500, (50, 450), 5)
     min_fat, max_fat = st.slider("Min to max fats", 0, 500, (50, 450), 5)
 
-    if min_protein > max_protein or min_carbs > max_carbs or min_cal > max_cal or min_fat > max_fat:
+    if min_protein >= max_protein or min_carbs >= max_carbs or min_cal >= max_cal or min_fat >= max_fat:
         st.error("Minimum nutrients must be less than maximum nutrients.")
     else:
         if st.button("Get recipe(s)"):
             recipes = rs.requestRecipeByNutrients(min_carbs=min_carbs, max_carbs=max_carbs, min_protein=min_protein, max_protein=max_protein, min_calories=min_cal, max_calories=max_cal, min_fat=min_fat, max_fat=max_fat, number_of_recipes=2)
-    
+
             for info, steps in recipes:
                 full_info = rs.requestRecipeInformation(info["id"])
                 info_col, steps_col = st.columns(2)
@@ -116,6 +124,7 @@ def showByNutrients():
                 info_col.subheader(info.get("title", "No title"))
                 info_col.image(info.get("image", ""), use_container_width=True)
 
+                # List the required ingredients
                 info_col.markdown("📝 Ingredients")
                 if "extendedIngredients" in full_info:
                     for ing in full_info["extendedIngredients"]:
@@ -140,6 +149,7 @@ def showByNutrients():
                 else:
                     steps_col.write("No instructions available.")
 
+# Display the fields depending on the search option
 if search_options == "by ingredients":
     showByIngredients()
 elif search_options == "by nutrients":
